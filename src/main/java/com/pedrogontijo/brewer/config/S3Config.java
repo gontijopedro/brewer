0f004bf3ada9d545db51d6cd332909b752ff204d
unknown <phpedrenrique@gmail.com>
2020-04-09 16:35:47 -0300

    projeto com erro deploy local

--- src/main/java/com/pedrogontijo/brewer/config/S3Config.java
+++ src/main/java/com/pedrogontijo/brewer/config/S3Config.java
@@ -1,10 +1,10 @@
 package com.pedrogontijo.brewer.config;
 
-// import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-// import org.springframework.core.env.Environment;
+import org.springframework.core.env.Environment;
 
 import com.amazonaws.ClientConfiguration;
 import com.amazonaws.auth.AWSCredentials;
@@ -17,12 +17,12 @@ import com.amazonaws.services.s3.AmazonS3Client;
 @PropertySource(value = { "file://${HOME}/.brewer-s3.properties" }, ignoreResourceNotFound = true)
 public class S3Config {
 	
-//	@Autowired
-//	private Environment env;
-
+	@Autowired
+	private Environment env;
 	@Bean
 	public AmazonS3 amazonS3() {
-		AWSCredentials credenciais = new BasicAWSCredentials("AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY");
+		AWSCredentials credenciais = new BasicAWSCredentials(
+				env.getProperty("AWS_ACCESS_KEY_ID"), env.getProperty("AWS_SECRET_ACCESS_KEY"));
 		AmazonS3 amazonS3 = new AmazonS3Client(credenciais, new ClientConfiguration());
 		Region regiao = Region.getRegion(Regions.SA_EAST_1);
 		amazonS3.setRegion(regiao);
--- src/main/java/com/pedrogontijo/brewer/repository/Usuarios.java
+++ src/main/java/com/pedrogontijo/brewer/repository/Usuarios.java
@@ -4,13 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.pedrogontijo.brewer.model.Usuario;
 import com.pedrogontijo.brewer.repository.helper.usuario.UsuariosQueries;
 
 
-@Repository
 public interface Usuarios extends JpaRepository<Usuario, Long>, UsuariosQueries {
 
 	public Optional<Usuario> findByEmail(String email);
